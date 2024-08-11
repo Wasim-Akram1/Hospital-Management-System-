b9cc04878dd03136e24da95bc4a5763290a4be7c
Wasim Akram <142140707+Wasim-Akram1@users.noreply.github.com>
2024-08-12 02:05:21 +0530

    Add files via upload

---
+++ Banao/user_management/accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Password and Confirm Password do not match")
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user_type', 'profile_picture', 'address_line1', 'city', 'state', 'pincode']
---
+++ Banao/user_management/accounts/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    USER_TYPE_CHOICES = (
+        ('admin', 'Admin'),
+        ('doctor', 'Doctor'),
+        ('patient', 'Patient'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    address_line1 = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
